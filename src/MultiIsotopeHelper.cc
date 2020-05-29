475947aa0189ab01f754c8f9deb3c316f0a2ddb1
maxschalz <58850267+maxschalz@users.noreply.github.com>
2020-05-29 21:08:53 +0200

    Add calculation of separation factor to Helper

--- src/MultiIsotopeHelper.cc
+++ src/MultiIsotopeHelper.cc
@@ -34,3 +34,18 @@ double MultiUraniumAssayMass(Material::Ptr rsrc) {
   return u235_assay ;
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+std::map<int,double> CalculateSeparationFactor(double alpha_235) {
+  std::map<int,double> separation_factors;
+  
+  // We consider U-238 to be the key component hence the mass differences
+  // are calculated with respect to this isotope.
+  for (int isotope = 232; isotope < 239; isotope++) {
+    if (isotope != 237) {
+      double delta_mass = 238. - isotope;
+      double alpha = 1. + delta_mass*(alpha_235-1.) / (238.-235.);
+      separation_factors[isotope] = alpha;
+    }
+  }
+  return separation_factors;
+}
--- src/MultiIsotopeHelper.h
+++ src/MultiIsotopeHelper.h
@@ -1,6 +1,8 @@
 #ifndef MBMORE_SRC_MULTI_ISOTOPE_HELPER_H_
 #define MBMORE_SRC_MULTI_ISOTOPE_HELPER_H_
 
+#include <map>
+
 #include "cyclus.h"
 
 namespace mbmore {
@@ -8,6 +10,22 @@ namespace mbmore {
 double MultiUraniumAssayAtom(Material::Ptr rsrc);
 
 double MultiUraniumAssayMass(Material::Ptr rsrc);
+
+// @brief Calculate stage separation factor for all isotopes starting from
+//        the given U235 stage separation factor.
+// @params stage separation factor for U235
+// @returns a map containing the stage separation factors for all U isotopes
+//          with the keys being the isotopes' mass
+// @details This function calculates the stage separation factor for all 
+//          uranium isotopes (U-232 to U-238, excluding U-237) starting from 
+//          the U-235 stage separation factor that is passed as an argument.
+//          Note that the stage separation factor is defined as the ratio of
+//          abundance ratio in product to abundance ratio in tails. This 
+//          method follows Houston G. Wood 'Effects of Separation Processes
+//          on Minor Uranium Isotopes in Enrichment Cascades'. In: Science
+//          and Global Security, 16:26--36 (2008). ISSN: 0892-9882. 
+//          DOI: 10.1080/08929880802361796
+std::map<int,double> CalculateSeparationFactor(double alpha_235);
 
 } // namespace mbmore
 
