3a6633451a13ef9c7b0e1d40ebecf154f3ae17da
maxschalz <58850267+maxschalz@users.noreply.github.com>
2020-05-31 11:14:12 +0200

    Add composition getters to Helper class
    This commit adds new functions that get the compositions of cyclus::Material
    or cyclus::Composition objects for every isotope (both atomic and mass
    fractions).

--- src/MultiIsotopeHelper.cc
+++ src/MultiIsotopeHelper.cc
@@ -1,42 +1,80 @@
 # include "MultiIsotopeHelper.h"
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-double MultiUraniumAssayAtom(Material::Ptr rsrc) {
+double MultiIsotopeAssayAtom(Material::Ptr rsrc) {
+  return MultiIsotopeAtomFrac(rsrc, 235);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+double MultiIsotopeAssayMass(Material::Ptr rsrc) {
+  return MultiUraniumIsotopeMass(rsrc, 235);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+double MultiIsotopeAtomFrac(Composition::Ptr composition, int isotope) {
+  int nuc_id = (92*1000 + isotope) * 10000; // isotope to unique NucID
+  std::map<int, double> v = composition->atom();
+  compmath::Normalize(&v);
+  return v[nuc_id];
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+double MultiIsotopeAtomFrac(Material::Ptr rsrc, int isotope) {
   MatQuery mq(rsrc);
-  double u235_assay;
+  const int isotopes[6] = {232, 233, 234, 235, 236, 238};
+  double isotope_assay;
   double uranium_atom_frac = 0;
   
   // Get total uranium mole fraction, all non-uranium elements are not 
   // considered here as they are directly sent to the tails.
-  for (int u_isotope = 232; u_isotope < 239: u_isotope++) {
-    int nuc_id = (92*1000 + u_isotope) * 10000;
+  for (int i : isotopes) {
+    int nuc_id = (92*1000 + i) * 10000;
     uranium_atom_frac += mq.atom_frac(nuc_id);
+    if (i==isotope) {
+      isotope_assay = mq.atom_frac(nuc_id);
+    }
   }
-  u235_assay = mq.atom_frac(922350000) / uranium_atom_frac;
+  isotope_assay /= uranium_atom_frac;
   
-  return u235_assay ;
+  return isotope_assay ;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-double MultiUraniumAssayMass(Material::Ptr rsrc) {
+double MultiIsotopeMassFrac(Composition::Ptr composition, int isotope) {
+  int nuc_id = (92*1000 + isotope) * 10000; // isotope to unique NucID
+  std::map<int, double> v = composition->mass();
+  compmath::Normalize(&v);
+  return v[nuc_id];
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+double MultiIsotopeMassFrac(Material::Ptr rsrc, int isotope) {
   MatQuery mq(rsrc);
-  double u235_assay;
-  double uranium_atom_frac = 0;
+  const int isotopes[6] = {232, 233, 234, 235, 236, 238};
+  double isotope_assay;
+  double uranium_mass_frac = 0;
   
   // Get total uranium mass fraction, all non-uranium elements are not 
   // considered here as they are directly sent to the tails.
-  for (int u_isotope = 232; u_isotope < 239: u_isotope++) {
-    int nuc_id = (92*1000 + u_isotope) * 10000;
-    uranium_atom_frac += mq.mass_frac(nuc_id);
+  for (int i : isotopes) {
+    int nuc_id = (92*1000 + i) * 10000;
+    uranium_mass_frac += mq.mass_frac(nuc_id);
+    if (i==isotope) {
+      isotope_assay = mq.mass_frac(nuc_id);
+    }
   }
-  u235_assay = mq.mass_frac(922350000) / uranium_atom_frac;
+  isotope_assay /= uranium_atom_frac;
   
-  return u235_assay ;
+  return isotope_assay ;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 std::map<int,double> CalculateSeparationFactor(double alpha_235) {
   std::map<int,double> separation_factors;
+  
+  // Convert the product to feed separation factor to overall stage 
+  // separation factor.
+  alpha_235 *= alpha_235
   
   // We consider U-238 to be the key component hence the mass differences
   // are calculated with respect to this isotope.
--- src/MultiIsotopeHelper.h
+++ src/MultiIsotopeHelper.h
@@ -7,24 +7,25 @@
 
 namespace mbmore {
 
-double MultiUraniumAssayAtom(Material::Ptr rsrc);
+double MultiIsotopeAssayAtom(Material::Ptr rsrc);
+double MultiIsotopeAssayMass(Material::Ptr rsrc);
+double MultiIsotopeAtomFrac(Composition::Ptr rsrc, int isotope);
+double MultiIsotopeAtomFrac(Material::Ptr rsrc, int isotope);
+double MultiIsotopeMassFrac(Composition::Ptr rsrc, int isotope);
+double MultiIsotopeMassFrac(Material::Ptr rsrc, int isotope);
 
-double MultiUraniumAssayMass(Material::Ptr rsrc);
-
-// @brief Calculate stage separation factor for all isotopes starting from
-//        the given U235 stage separation factor.
-// @params stage separation factor for U235
-// @returns a map containing the stage separation factors for all U isotopes
-//          with the keys being the isotopes' mass
-// @details This function calculates the stage separation factor for all 
-//          uranium isotopes (U-232 to U-238, excluding U-237) starting from 
-//          the U-235 stage separation factor that is passed as an argument.
-//          Note that the stage separation factor is defined as the ratio of
-//          abundance ratio in product to abundance ratio in tails. This 
-//          method follows Houston G. Wood 'Effects of Separation Processes
-//          on Minor Uranium Isotopes in Enrichment Cascades'. In: Science
-//          and Global Security, 16:26--36 (2008). ISSN: 0892-9882. 
-//          DOI: 10.1080/08929880802361796
+// Calculates the stage separation factor for all isotopes starting from 
+// the given U235 product to feed separation factor.
+//
+// Returns a map containing the stage separation factors for all U isotopes
+// with the keys being the isotopes' mass.
+//
+// Note that the stage separation factor is defined as the ratio of 
+// abundance ratio in product to abundance ratio in tails. This method 
+// follows Houston G. Wood 'Effects of Separation Processes on Minor 
+// Uranium Isotopes in Enrichment Cascades'. In: Science and Global 
+// Security, 16:26--36 (2008). ISSN: 0892-9882.
+// DOI: 10.1080/08929880802361796
 std::map<int,double> CalculateSeparationFactor(double alpha_235);
 
 } // namespace mbmore
